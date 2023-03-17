994ed2ccf25bccd9b327681eaa158c502621afa3
Engineer V <victor.mecdoors@gmail.com>
2023-03-17 05:57:53 -0700

    a program that prints the lowercase alphabet in reverse, followed by a new line.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet reversed using putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+
+	return (0);
+}
