f50497e7c57339800c66064ce0e56c09752336ed
Engineer V <victor.mecdoors@gmail.com>
2023-03-23 05:22:20 -0700

    fixing betty style

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * * _isupper - checks for uppercase character
- * * @c: the character to be checked
- * * Return: 1 if c is uppercase, 0 otherwise
- * */
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
 {
 	return (c >= 'A' && c <= 'Z');
