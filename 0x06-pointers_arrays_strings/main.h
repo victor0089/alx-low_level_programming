a886decb591102534757accb8111e06726886cec
Engineer V <victor.mecdoors@gmail.com>
2023-03-29 13:08:48 -0700

    testing the code

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_touper(char *a);
+char *string_toupper(char *a);
 char *cap_string(char *a);
 char *leet(char *a);
 char *rot13(char *a);
