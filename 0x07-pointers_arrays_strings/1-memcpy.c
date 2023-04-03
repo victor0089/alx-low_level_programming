67394afc77a7149513c404c50c265d5025c04ce3
Engineer V <victor.mecdoors@gmail.com>
2023-04-03 12:01:01 -0700

    adding 2nd pro

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: memory area to copy from
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int k;
+
+	for (k = 0; k < n; k++)
+	{
+		dest[k] = src[k];
+	}
+
+	return (dest);
+}
