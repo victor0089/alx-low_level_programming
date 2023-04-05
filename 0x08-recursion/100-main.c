4bb5c7242dfd1e539d326908689da961499cea83
Engineer V <victor.mecdoors@gmail.com>
2023-04-05 21:38:25 +0200

    adding main files

---
+++ 0x08-recursion/100-main.c
@@ -0,0 +1,19 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = is_palindrome("level");
+    printf("%d\n", r);
+    r = is_palindrome("redder");
+    printf("%d\n", r);
+    r = is_palindrome("test");
+    printf("%d\n", r);
+    r = is_palindrome("step on no pets");
+    printf("%d\n", r);
+    return (0);
+}
