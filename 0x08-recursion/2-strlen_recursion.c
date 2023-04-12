7749b387760e7d5746a36a547122b5022be6b4d3
Engineer V <victor.mecdoors@gmail.com>
2023-04-12 03:26:17 -0700

    fixing  description

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen_recursion - returns the length of a string
- * @s: string to calculate the length of
+ * @z: string to calculate the length of
  *
  * Return: length of the string
  */
