605208706ec9afe7dab0115f02cff0f824e5f6b9
Engineer V <victor.mecdoors@gmail.com>
2023-04-12 01:54:51 -0700

    re edit

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion - returns the value of b raised to the power of c
+ * _pow_recursion - returns the value of x raised to the power of y
  * @x: value to raise
  * @y: power
  *
