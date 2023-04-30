8f6a3b6e2cc29f62acee15492c25241a06fcab38
Victor <victor.mecdoors@gmail.com>
2023-04-30 03:30:01 -0700

    fixing 0-print_listint file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,17 +5,17 @@
  * @h: linked list of type listint_t to priNt
  *
  * Return:NnMbEr oV NOOOdeSSS
- * */
+ */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t number = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		num++;
+		number++;
 		h = h->next;
 	}
 
-	return (num);
+	return (number);
 }
