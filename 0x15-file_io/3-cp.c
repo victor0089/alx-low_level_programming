cb17ee62c0dbace278c9eb2b987a202dd3014684
Victor <victor.mecdoors@gmail.com>
2023-05-08 15:49:07 -0700

    fixing

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -46,7 +46,8 @@ void close_file(int fd)
 
 /**
  * main - Copies the contents of a file to another file.
- * @argc: The number of arguments supplied to the program.* * @argv: An array of pointers to the arguments.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
  *
