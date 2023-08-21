876a0059554368fc87b4bddd05a469a8811a8a24
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-08-21 23:33:38 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/victor0089/alx-low_level_programming/raw/master/0x18-dynamic_libraries/100-operations.so
-export LD_PRELOAD=/tmp/100-operations.so
+wget -P /tmp https://github.com/victor0089/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
