a5441041922b0bc9a2d1d2810b2a56df80c4769a
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-12-12 19:37:01 +0200

    Update 0-main.c

--- 0x1E-search_algorithms/0-main.c
+++ 0x1E-search_algorithms/0-main.c
@@ -19,27 +19,3 @@ int main(void)
     printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
     return (EXIT_SUCCESS);
 }
-wilfried@0x1E-search_algorithms$ gcc -Wall -Wextra -Werror -pedantic -std=gnu89 0-main.c 0-linear.c -o 0-linear
-wilfried@0x1E-search_algorithms$ ./0-linear 
-Value checked array[0] = [10]
-Value checked array[1] = [1]
-Value checked array[2] = [42]
-Value checked array[3] = [3]
-Found 3 at index: 3
-
-Value checked array[0] = [10]
-Value checked array[1] = [1]
-Value checked array[2] = [42]
-Found 42 at index: 2
-
-Value checked array[0] = [10]
-Value checked array[1] = [1]
-Value checked array[2] = [42]
-Value checked array[3] = [3]
-Value checked array[4] = [4]
-Value checked array[5] = [42]
-Value checked array[6] = [6]
-Value checked array[7] = [7]
-Value checked array[8] = [-1]
-Value checked array[9] = [9]
-Found 999 at index: -1
