63022450777b90df873135ff18ca8d25b30ffef9
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-12-12 19:07:08 +0200

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-
 /**
  * linear_search - searchesforavalue in anarray
  * integers using the Linear searchalgorithm
@@ -10,14 +9,14 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
-	if (array == NULL)
-		return (-1);
-	for (i = 0; i < (int)size; i++)
-	{
-		printf("Value checked array[%u] = [%d]\n", i, array[i]);
-		if (value == array[i])
-			return (i);
-	}
-	return (-1);
+int i;
+if (array == NULL)
+return (-1);
+for (i = 0; i < (int)size; i++)
+{
+printf("Value checked array[%u] = [%d]\n", i, array[i]);
+if (value == array[i])
+return (i);
+}
+return (-1);
 }
