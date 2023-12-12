ada4ff76d2a09a1f137f28ca7c81c32fdb520054
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-12-12 19:13:40 +0200

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,6 +1,5 @@
 #include "search_algos.h"
 #include <math.h>
-
 /**
  * jump_search - searches for avalue inan array
  * integers using the Jumpsearch algorithm
@@ -11,26 +10,26 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int index, m, k, prev;
-	if (array == NULL || size == 0)
-		return (-1);
-	m = (int)sqrt((double)size);
-	k = 0;
-	prev = index = 0;
-	do {
-		printf("Value checked array[%d] = [%d]\n", index, array[index]);
-		if (array[index] == value)
-			return (index);
-		k++;
-		prev = index;
-		index = k * m;
-	} while (index < (int)size && array[index] < value);
-	printf("Value found between indexes [%d] and [%d]\n", prev, index);
-	for (; prev <= index && prev < (int)size; prev++)
-	{
-		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
-		if (array[prev] == value)
-			return (prev);
-	}
-	return (-1);
+int index, m, k, prev;
+if (array == NULL || size == 0)
+return (-1);
+m = (int)sqrt((double)size);
+k = 0;
+prev = index = 0;
+do {
+printf("Value checked array[%d] = [%d]\n", index, array[index]);
+if (array[index] == value)
+return (index);
+k++;
+prev = index;
+index = k * m;
+} while (index < (int)size && array[index] < value);
+printf("Value found between indexes [%d] and [%d]\n", prev, index);
+for (; prev <= index && prev < (int)size; prev++)
+{
+printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+if (array[prev] == value)
+return (prev);
+}
+return (-1);
 }
