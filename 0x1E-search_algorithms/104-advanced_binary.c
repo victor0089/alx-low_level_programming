2cadb42fe1f64afc608db9bfd2c4a2e77fb76d08
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-12-12 19:09:43 +0200

    Update 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-
 /**
  * rec_search - searches for avalue in anarray
  * integers using theBinary searchalgorithm
@@ -10,26 +9,26 @@
  */
 int rec_search(int *array, size_t size, int value)
 {
-	size_t half = size / 2;
-	size_t i;
-	if (array == NULL || size == 0)
-		return (-1);
-	printf("Searching in array");
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-	printf("\n");
-	if (half && size % 2 == 0)
-		half--;
-	if (value == array[half])
-	{
-		if (half > 0)
-			return (rec_search(array, half + 1, value));
-		return ((int)half);
-	}
-	if (value < array[half])
-		return (rec_search(array, half + 1, value));
-	half++;
-	return (rec_search(array + half, size - half, value) + half);
+size_t half = size / 2;
+size_t i;
+if (array == NULL || size == 0)
+return (-1);
+printf("Searching in array");
+for (i = 0; i < size; i++)
+printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+printf("\n");
+if (half && size % 2 == 0)
+half--;
+if (value == array[half])
+{
+if (half > 0)
+return (rec_search(array, half + 1, value));
+return ((int)half);
+}
+if (value < array[half])
+return (rec_search(array, half + 1, value));
+half++;
+return (rec_search(array + half, size - half, value) + half);
 }
 /**
  * advanced_binary - calls to rec_searchtoreturn
@@ -41,9 +40,9 @@ int rec_search(int *array, size_t size, int value)
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	int index;
-	index = rec_search(array, size, value);
-	if (index >= 0 && array[index] != value)
-		return (-1);
-	return (index);
+int index;
+index = rec_search(array, size, value);
+if (index >= 0 && array[index] != value)
+return (-1);
+return (index);
 }
